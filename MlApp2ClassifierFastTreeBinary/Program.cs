ceb5df0e55ef3abdd043cf9b7247da708e78da75
f4tp <mr_treadwell@live.co.uk>
2018-09-30 12:05:26 +0100

    Add project files.

---
+++ MlApp2ClassifierFastTreeBinary/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MlApp2ClassifierFastTreeBinary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
